105200989ca0282053e34943377f2c9cb879d15c
Mario <mario_scipio@hotmail.es>
2021-10-16 10:24:42 +0200

    Leave button

--- Quiz/app/src/main/java/com/example/quiz_mario_borja/Start_Activity.java
+++ Quiz/app/src/main/java/com/example/quiz_mario_borja/Start_Activity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class Start_Activity extends AppCompatActivity {
 
-    private Button startButton;
+    private Button startButton,leaveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +17,25 @@ public class Start_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         startButton = findViewById(R.id.jugar_button);
+        leaveButton = findViewById(R.id.salir_button);
 
         Intent intent = new Intent(Start_Activity.this, MainActivity.class);
 
+        // empezar a jugar
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(intent);
+            }
+        });
+
+        // salir de la aplicación
+        leaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveTaskToBack(true); // necesario para que el botón no devuelva a la actividad anterior
+                // finish();
+                System.exit(1); // también puede usarse finish(), pero esto detiene la ejecucion
             }
         });
     }
