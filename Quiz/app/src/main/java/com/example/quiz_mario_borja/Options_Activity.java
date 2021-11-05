353ce319a8280e379b8331254e937cfd6a8b613b
Mario <mario_scipio@hotmail.es>
2021-11-05 11:39:30 +0100

    Options menu layout

---
+++ Quiz/app/src/main/java/com/example/quiz_mario_borja/Options_Activity.java
@@ -0,0 +1,14 @@
+package com.example.quiz_mario_borja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Options_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
