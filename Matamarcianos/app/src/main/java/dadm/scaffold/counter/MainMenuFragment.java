79c6baa68259c1e0944d275a8ea6fccdb0aed442
Mario <mario_scipio@hotmail.es>
2022-01-03 10:30:50 +0100

    Botón de salir en el menu principal

--- Matamarcianos/app/src/main/java/dadm/scaffold/counter/MainMenuFragment.java
+++ Matamarcianos/app/src/main/java/dadm/scaffold/counter/MainMenuFragment.java
@@ -15,7 +15,7 @@ import dadm.scaffold.R;
 import dadm.scaffold.ScaffoldActivity;
 
 
-public class MainMenuFragment extends BaseFragment implements View.OnClickListener {
+public class MainMenuFragment extends BaseFragment /*implements View.OnClickListener*/ {
     public MainMenuFragment() {
     }
 
@@ -29,11 +29,23 @@ public class MainMenuFragment extends BaseFragment implements View.OnClickListen
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.btn_start).setOnClickListener(this);
+        view.findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ScaffoldActivity)getActivity()).startGame();
+            }
+        });
+        view.findViewById(R.id.btn_quit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(1);
+            }
+        });
 
         Animation pulseAnimation = AnimationUtils.loadAnimation(getActivity(),
                 R.anim.button_pulse);
         view.findViewById(R.id.btn_start).startAnimation(pulseAnimation);
+        view.findViewById(R.id.btn_quit).startAnimation(pulseAnimation);
 
         Animation titleAnimation = AnimationUtils.loadAnimation(getActivity(),
                 R.anim.title_enter);
@@ -43,9 +55,9 @@ public class MainMenuFragment extends BaseFragment implements View.OnClickListen
                 R.anim.subtitle_enter);
         view.findViewById(R.id.GameSubtitle).startAnimation(subtitleAnimation);
     }
-
+/*
     @Override
     public void onClick(View v) {
         ((ScaffoldActivity)getActivity()).startGame();
-    }
+    }*/
 }
